cbdca218085c7f9b2a03cefabc090b3d9b045a57
Pablo Flores <pabloff5545@gmail.com>
2023-11-07 15:34:40 +0100

    setup

--- G13.r
+++ G13.r
@@ -3,3 +3,22 @@
 # Jiapeng Ma: s2592596
 
 # Contributions:
+library(datasets)
+data(iris)
+
+d <- c(4, 8, 7, 3)
+netup <- function(d){
+  
+}
+
+forward <- function(nn, inp){
+  
+}
+
+backward <- function(nn, k){
+  
+}
+
+train <- function(nn, inp, k, eta=0.1, mb=10, nstep=10000){
+  
+}
