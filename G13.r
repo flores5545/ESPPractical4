f9b5fd28ca764a199820eb9671f3469e92a60142
Pablo Flores <pabloff5545@gmail.com>
2023-11-15 21:09:06 +0100

    fixed error in backprop, added comments

--- G13.r
+++ G13.r
@@ -6,36 +6,49 @@
 library(datasets)
 data(iris)
 
-d <- c(4, 8, 7, 3)
+# This file contains code to train a neural network for classification using
+# Stochastic Gradient  Descent as the optimisation method to update the 
+# hyperparameters of the network. First, we write a function to represent a  
+# neural network given a vector with the length of each layer. Then, we code a 
+# function to perform a forward pass of the neural network, updating the value 
+# of each node according to some given input, using the ReLU activation function
+# as the nonlinearity. Moreover, we implement a function to perform a backward 
+# pass of the neural network, where we compute the derivatives of the loss
+# function. For this project, we use the Softmax function to compute the 
+# probability that the output variable is in a given class, and the 
+# cross-entropy loss function, as often used in classification problems
+
+
 netup <- function(d){
-  # This function will return a list presenting the network
+  # This function will return a list representing the neural network
   # Input: 
   #   d: a vector giving the number of nodes in each layer of a network
   # Output:
   #   a list representing the network, including:
-  #     h: a list of nodes for each layer. h[[l]] should be a vector of length d[l],
-  #       which will contain the node values for layer l.
-  #     W: a list of weight matrices. W[[l]] is the weight matrix linking layer l to layer l+1. 
-  #       Initialize the elements with U (0, 0.2) random deviates.
-  #     b: a list of offset vectors. b[[l]] is the offset vector linking layer l to layer l+1. 
-  #       Initialize the elements with U (0, 0.2) random deviates.
+  #     h: a list of nodes for each layer. h[[l]] should be a vector of length 
+  #       d[l], which will contain the node values for layer l
+  #     W: a list of weight matrices. W[[l]] is the weight matrix linking layer   
+  #       l to layer l+1. Initialize the elements with U(0, 0.2) random deviates
+  #     b: a list of offset vectors. b[[l]] is the offset vector linking layer l  
+  #      to layer l+1. Initialize the elements with U(0, 0.2) random deviates
   
   # Initialize the nodes, weights, and offset vector
   h <- vector("list", length = length(d))
   W <- vector("list", length = length(d) - 1)
   b <- vector("list", length = length(d) - 1)
   
-  for (l in 1:length(d)) {
+  for (l in 1:length(d)) { # Initialize the nodes corresponding to the given d
     h[[l]] <- rep(0, d[l])
   }
   
-  # Initialize the elements with U (0, 0.2) random deviates
+  # Initialize the elements of the weight matrices and the offset vectors with 
+  # U(0, 0.2) random deviates
   for (l in 1:(length(d) - 1)) {
     W[[l]] <- matrix(runif(d[l] * d[l + 1], 0, 0.2), nrow = d[l], ncol = d[l + 1])
     b[[l]] <- runif(d[l + 1], 0, 0.2)
   }
   
-  nn <- list(h=h, W=W, b=b)
+  nn <- list(h=h, W=W, b=b) # List representing the neural network
   return (nn)
 }
 
@@ -45,16 +58,19 @@ relu <- function(x) {
 }
 
 forward <- function(nn, inp){
-  # This function computes the remaining node values implied by inp, and return the updated network list 
+  # This function computes the remaining node values implied by the given input
+  # and returns the updated network list 
   # Input:
   #   nn is a network list as returned by netup
-  #   inp a vector of input values for the first layer. 
+  #   inp a vector of input values for the first layer
   # Output:
-  #   return the updated network list (as the only return object).
-  nn$h[[1]] <- inp
+  #   return the updated network list (as the only return object)
+  
+  nn$h[[1]] <- inp # Set the value of the nodes in the first layer
+  
   # Perform the forward pass through the network
   for (l in 2:length(nn$h)) {
-    # Compute the weighted sum of inputs and apply ReLU activation function
+    # Compute the weighted sum of inputs and apply the ReLU activation function
     nn$h[[l]] <- relu(nn$h[[l - 1]] %*% nn$W[[l - 1]] + nn$b[[l - 1]])
   }
   return (nn)
@@ -62,13 +78,15 @@ forward <- function(nn, inp){
 
 
 backward <- function(nn, k) {
-  # This function computes the derivatives of the loss corresponding to output class k for network nn (returned from forward)
+  # This function computes the derivatives of the loss function corresponding to
+  # output class k for network nn (returned from forward)
   # Input:
   #   nn: network, returned from forward
   #   k: output class
   # Output:
   #   A list of updated list including:
-  #     dh, dW and db, which are the derivatives w.r.t the nodes, weights and offsets, respectively
+  #     dh, dW and db, which are the derivatives w.r.t the nodes, weights and 
+  #     offsets, respectively
   
   # Number of layers
   n_layers <- length(nn$h)
@@ -84,26 +102,29 @@ backward <- function(nn, k) {
   dh[[n_layers]] <- probabilities
   dh[[n_layers]][k] <- dh[[n_layers]][k] - 1  # Subtract 1 only from the true class k
   
-  # Initialize storage for gradients of weights and biases
+  # Initialize the derivative of the loss w.r.t. weights and biases
   dW <- vector("list", length = n_layers - 1)
   db <- vector("list", length = n_layers - 1)
   
-  # Backpropagate the error through the layers
+  # Perform the backward pass through the network
   for (l in seq(n_layers - 1, 1, by = -1)) {
-    # Compute gradients for biases directly from dh of the next layer
-    db[[l]] <- dh[[l + 1]]
-    db[[l]][nn$h[[l]] <= 0] <- 0  # Applying ReLU derivative
     
-    # Compute gradients for weights as the outer product of dh and the activations from the current layer
-    dW[[l]] <- nn$h[[l]] %*% t(dh[[l + 1]])
-    dW[[l]][nn$h[[l]] <= 0] <- 0  # Applying ReLU derivative
+    # Recall that ReLU was used as the activation function
+    # Compute derivatives with respect to biases directly from dh of the next layer
+    db[[l]] <- dh[[l + 1]]
+    db[[l]][nn$h[[l]] <= 0] <- 0  
+    
+    # Compute gradients for weights as the outer product of dh of the next layer
+    # and the node values from the current layer
+    dW[[l]] <- dh[[l + 1]] %*% t(nn$h[[l]]) 
+    dW[[l]][nn$h[[l]] <= 0] <- 0
 
     # Update dh for the previous layer using the chain rule
     dh[[l]] <- t(nn$W[[l]]) %*% dh[[l + 1]]
-    dh[[l]][nn$h[[l]] <= 0] <- 0  # Applying ReLU derivative
+    dh[[l]][nn$h[[l]] <= 0] <- 0
   }
   
-  # Store the gradients in the network list
+  # Store the derivatives in the network list
   nn$dW <- dW
   nn$db <- db
   nn$dh <- dh 
@@ -160,7 +181,8 @@ train <- function(nn, inp, k, eta = 0.01, mb = 10, nstep = 10000) {
     
     # Update weights and biases
     for (l in 1:length(nn$W)) {
-      
+      nn$W[[l]] <- nn$W[[l]] - eta * (1/mb) * gradients_W[[l]]
+      nn$b[[l]] <- nn$b[[l]] - eta * (1/mb) * gradients_b[[l]]
     }
     
     # print out the loss to monitor training every few steps
@@ -183,6 +205,8 @@ train <- function(nn, inp, k, eta = 0.01, mb = 10, nstep = 10000) {
 # where the test data consists of every 5th row of the iris dataset, starting from row 5
 # set the seed to provide an example in which training has worked and the loss has been substantially reduced from pre- to post-training
 #
+
+d <- c(4, 8, 7, 3)
 library(datasets)
 data(iris) 
 
